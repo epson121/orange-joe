6784808ab8e1f94dba04796f11d8f1a129c6d97f
Luka Rajcevic <traxdater12@gmail.com>
2023-04-27 12:17:53 +0200

    Simple framework setup

---
+++ roberto/public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// framework/front.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+$map = [
+    '/hello' => __DIR__.'/../src/hello.php',
+    '/bye'   => __DIR__.'/../src/bye.php',
+];
+
+$path = $request->getPathInfo();
+if (isset($map[$path])) {
+    ob_start();
+    require $map[$path];
+    $response->setContent(ob_get_clean());
+} else {
+    $response->setStatusCode(404);
+    $response->setContent('Not Found');
+}
+
+$response->send();
